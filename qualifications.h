a10641604be24925d5e549f77097bf2b44b9bf8f
TheGingerNut <ljmc2000@gmail.com>
2024-04-29 16:34:56 +0100

    defancified education because scraper bots don't handle the images well

--- qualifications.h
+++ qualifications.h
@@ -2,9 +2,9 @@
 #define QUALIFICATIONS_H
 
 var qualifications = [
-	{caption: "BSc from Technological University of Dublin (Formerly DIT)", preview: "Degree.jpg"},
-	{caption: "KNIME L3 Certification", preview: "knime_l3.jpg"},
-	{caption: "Leaving Certificate from St. Josephs CBS", preview: "LC.jpeg"},
+	{certificate: "KNIME L3 Certificate", institution: "KNIME AG, Zurich, Switzerland", date: "April 2021"},
+	{certificate: "Bachelor of Science", institution: "Technological University of Dublin", date: "Sep 2016 - Jun 2020", grade: "First Class Honors"},
+	{certificate: "Leaving Certificate", institution: "St. Josephs CBS", date:"Sep 2010 - Jun 2016", grade: "410 points"},
 ]
 
 #endif
