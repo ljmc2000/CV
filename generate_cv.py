cdaae566d1371108189f0fc1413e25066ad1b8a2
Delilah McCormick <ljmc2000@gmail.com>
2024-08-26 18:46:57 +0100

    added experience section

--- generate_cv.py
+++ generate_cv.py
@@ -5,6 +5,9 @@ from img_utils import image, svg
 with open('personal_details.yaml') as personal_details_src:
 	personal_details=yaml.safe_load(personal_details_src)
 
+with open('experience.yaml') as experience_src:
+	experience=yaml.safe_load(experience_src)
+
 outfile=open("LiamMcCormick_CV.html", "w+")
 outfile.write('<html>')
 
@@ -22,17 +25,31 @@ outfile.write('</head><body>')
 
 #about me
 outfile.write(f'''
-<div style="display: inline-flex">{image('me2023.jpg',136)}<div class="aboutme invisible-links"><div style="font-size: 24px; padding-bottom: 6px">{personal_details["name"]}</div>''')
+<div style="display: inline-flex">{image('me2023.jpg',136)}<div class="aboutme invisible_links"><div style="font-size: 24px; padding-bottom: 6px">{personal_details["name"]}</div>''')
 
 for link, label, icon in [
 	('mailto:'+personal_details["email"], personal_details["email"], 'email-1572-svgrepo-com.svg'),
 	('https://github.com/ljmc2000', 'github.com/ljmc2000', 'github.svg'),
 	(personal_details['linkedin_final'], personal_details['linkedin'], 'iconmonstr-linkedin-3.svg'),
 	]:
-	outfile.write(f'''<div><a href={link}>{image(icon,16,scale=2)} {label}''')
-outfile.write(f'''<div>{image('cell-phone-svgrepo-com.svg',16,scale=2)} {personal_details["cellnumber"]}''')
+	outfile.write(f'''<div><a href={link}>{image(icon,16,scale=2)} {label}</a></div>''')
+outfile.write(f'''<div>{image('cell-phone-svgrepo-com.svg',16,scale=2)} {personal_details["cellnumber"]}</div>''')
 
 outfile.write('</div></div>')
+
+#Experience
+outfile.write('<div class="section_header">Experience</div>')
+for company, details in experience.items():
+	outfile.write(f'''
+	<div class="job_outline">
+		<b>{details["start_date"]} to {details["end_date"]}:</b> {details["title"]} at {company}
+	</div>''')
+
+	if duties:=details.get('duties'):
+		outfile.write('<ul class="job_details">')
+		for duty in duties:
+			outfile.write(f'<li>{duty}</li>')
+		outfile.write('</ul>')
 
 #end
 outfile.write('</body></html>')
