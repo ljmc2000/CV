124b70ccad035b0f1baded605ab4060a2e7b5573
Delilah McCormick <ljmc2000@gmail.com>
2024-09-01 20:51:51 +0100

    handle all image assets the same when in compress mode

--- generate_cv.py
+++ generate_cv.py
@@ -1,7 +1,7 @@
 import yaml
 
 from decorations import li
-from img_utils import image, skill, svg
+from img_utils import image, skill
 
 with open('experience.yaml') as experience_src:
 	experience=yaml.safe_load(experience_src)
@@ -39,8 +39,8 @@ for link, label, icon in [
 	('https://github.com/ljmc2000', 'github.com/ljmc2000', 'github.svg'),
 	(personal_details['linkedin_final'], personal_details['linkedin'], 'iconmonstr-linkedin-3.svg'),
 	]:
-	outfile.write(f'''<div><a href={link}>{svg(icon,16)} {label}</a></div>''')
-outfile.write(f'''<div>{svg('cell-phone-svgrepo-com.svg',16)} {personal_details["cellnumber"]}</div>''')
+	outfile.write(f'''<div><a href={link}>{image(icon,16)} {label}</a></div>''')
+outfile.write(f'''<div>{image('cell-phone-svgrepo-com.svg',16)} {personal_details["cellnumber"]}</div>''')
 
 outfile.write('</div></div>')
 
--- img_utils.py
+++ img_utils.py
@@ -12,28 +12,37 @@ def aspect_ratio(filename: str) -> float:
 	w,h=check.stdout.decode().split(':')
 	return float(w)/float(h)
 
-def image(filename: str, height: int, width: int=None, *, scale: int=1, class_names='', style='') -> str:
-	if not width:
-		width=int(height*aspect_ratio(filename))
-
-	dimensions=f'{width}x{height}'
-	class_names=f'class="{class_names}" ' if class_names else ""
-	style=f'style="{style}" ' if style else ""
-
-	imgmck=subprocess.run(['magick', asset(filename), '-resize', f'{scale*BASE_SCALE*width}x{scale*BASE_SCALE*height}', 'JPG:-'], stdout=subprocess.PIPE)
-	return f'''<img {class_names}{style} width="{width}" height="{height}" src="data: image/jpeg; base64, {base64.b64encode(imgmck.stdout).decode()}">'''
-
 if COMPRESS_IMAGES:
+	def image(filename: str, height: int, width: int=None, *, scale: int=1, class_names='', style='') -> str:
+		if not width:
+			width=int(height*aspect_ratio(filename))
+
+		dimensions=f'{width}x{height}'
+		class_names=f'class="{class_names}" ' if class_names else ""
+		style=f'style="{style}" ' if style else ""
+
+		imgmck=subprocess.run(['magick', asset(filename), '-resize', f'{scale*BASE_SCALE*width}x{scale*BASE_SCALE*height}', 'JPG:-'], stdout=subprocess.PIPE)
+		return f'''<img {class_names}{style} width="{width}" height="{height}" src="data: image/jpeg; base64, {base64.b64encode(imgmck.stdout).decode()}">'''
+
 	def skill(skill_name: str) -> str:
-		imgmck=subprocess.run(['magick', f'skill_icons/{skill_name}.svg', '-geometry', 'x24', 'JPG:-'], stdout=subprocess.PIPE)
+		imgmck=subprocess.run(['magick', '-background', 'transparent', f'skill_icons/{skill_name}.svg', '-geometry', f'x{12*BASE_SCALE}', 'WEBP:-'], stdout=subprocess.PIPE)
 
 		if imgmck.returncode==0:
-			return f'<button class="skill_badge"><img class="skill_badge_img" src="data: image/jpeg; base64, {base64.b64encode(imgmck.stdout).decode()}"></button>'
+			return f'<button class="skill_badge"><img class="skill_badge_img" src="data: image/png; base64, {base64.b64encode(imgmck.stdout).decode()}"></button>'
 
 		else:
 			return f'<button class="skill_badge"><img><span class="skill_badge_txt">{skill_name}</span></button>'
 
 else:
+	def image(filename: str, height: int, width: int=None, *, scale: int=1, class_names='', style='') -> str:
+		mimetype=subprocess.run(['file', '-b', '--mime-type', asset(filename)], stdout=subprocess.PIPE)
+		width=f'width={width} ' if width else ""
+		class_names=f'class="{class_names}" ' if class_names else ""
+		style=f'style="{style}" ' if style else ""
+
+		with open(asset(filename), 'rb') as f:
+			return f'<img {class_names}{style}{width} height="{height}" src="data: {mimetype.stdout.decode()}; base64, {base64.b64encode(f.read()).decode()}">'
+
 	def skill(skill_name: str) -> str:
 		try:
 			with open(f'skill_icons/{skill_name}.svg', 'rb') as skill_icon_src:
@@ -41,8 +50,3 @@ else:
 
 		except FileNotFoundError:
 			return f'<button class="skill_badge"><img><span class="skill_badge_txt">{skill_name}</span></button>'
-
-
-def svg(filename: str, height: int) -> str:
-	with open(asset(filename), 'rb') as f:
-		return f'<img height="{height}" src="data: image/svg+xml; base64, {base64.b64encode(f.read()).decode()}">'
