5208517432a4a6b586f8dd72938429fd76f59b8a
Delilah McCormick <ljmc2000@gmail.com>
2024-09-02 16:37:43 +0100

    fixed env provided base scale interpreted as string not float plus elimintate second call to magick to check geometry before conversion

--- img_utils.py
+++ img_utils.py
@@ -1,28 +1,29 @@
 import base64, subprocess
 from os import environ
 
-BASE_SCALE=environ.get('BASE_SCALE', 1.75)
+BASE_SCALE=float(environ.get('BASE_SCALE', 1.75))
 COMPRESS_IMAGES=True if environ.get('COMPRESS_IMAGES', 0) else False
 
 def asset(filename):
 	return 'assets/'+filename
 
-def aspect_ratio(filename: str) -> float:
-	check=subprocess.run(["magick", "identify", "-format", "%w:%h", asset(filename)], stdout=subprocess.PIPE)
-	w,h=check.stdout.decode().split(':')
-	return float(w)/float(h)
-
 if COMPRESS_IMAGES:
 	def image(filename: str, height: int, width: int=None, *, scale: int=1, class_names='', style='') -> str:
-		if not width:
-			width=int(height*aspect_ratio(filename))
+		if width:
+			w=f'{scale*BASE_SCALE*width}'
+			width=f'width="{width}" '
+		else:
+			w=''
+			width=''
 
-		dimensions=f'{width}x{height}'
+		h=f'{scale*BASE_SCALE*height}'
+		height=f'height="{height}" '
+
 		class_names=f'class="{class_names}" ' if class_names else ""
 		style=f'style="{style}" ' if style else ""
 
-		imgmck=subprocess.run(['magick', asset(filename), '-resize', f'{scale*BASE_SCALE*width}x{scale*BASE_SCALE*height}', 'JPG:-'], stdout=subprocess.PIPE)
-		return f'''<img {class_names}{style} width="{width}" height="{height}" src="data: image/jpeg; base64, {base64.b64encode(imgmck.stdout).decode()}">'''
+		imgmck=subprocess.run(['magick', asset(filename), '-geometry', f'{w}x{h}', 'JPG:-'], stdout=subprocess.PIPE)
+		return f'''<img {class_names}{style}{width}{height} src="data: image/jpeg; base64, {base64.b64encode(imgmck.stdout).decode()}">'''
 
 	def skill(skill_name: str) -> str:
 		imgmck=subprocess.run(['magick', '-background', 'transparent', f'skill_icons/{skill_name}.svg', '-geometry', f'x{16*BASE_SCALE}', 'WEBP:-'], stdout=subprocess.PIPE)
@@ -37,11 +38,12 @@ else:
 	def image(filename: str, height: int, width: int=None, *, scale: int=1, class_names='', style='') -> str:
 		mimetype=subprocess.run(['file', '-b', '--mime-type', asset(filename)], stdout=subprocess.PIPE)
 		width=f'width={width} ' if width else ""
+		height=f'height="{height}" '
 		class_names=f'class="{class_names}" ' if class_names else ""
 		style=f'style="{style}" ' if style else ""
 
 		with open(asset(filename), 'rb') as f:
-			return f'<img {class_names}{style}{width} height="{height}" src="data: {mimetype.stdout.decode()}; base64, {base64.b64encode(f.read()).decode()}">'
+			return f'<img {class_names}{style}{width}{height} src="data: {mimetype.stdout.decode()}; base64, {base64.b64encode(f.read()).decode()}">'
 
 	def skill(skill_name: str) -> str:
 		try:
