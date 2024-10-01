df8b73160e684f7a12600909583ff42ac28b8694
Delilah McCormick <ljmc2000@gmail.com>
2024-10-01 22:48:01 +0100

    use css for image heights

--- img_utils.py
+++ img_utils.py
@@ -11,19 +11,19 @@ if IMAGE_SCALE:=float(environ.get('IMAGE_SCALE',0)):
 	def image(filename: str, height: int, width: int=None, *, scale: int=1, class_names='', style='') -> str:
 		if width:
 			w=f'{scale*IMAGE_SCALE*width}'
-			width=f'width="{width}" '
+			width=f'width: {width}px;'
 		else:
 			w=''
 			width=''
 
 		h=f'{scale*IMAGE_SCALE*height}'
-		height=f'height="{height}" '
+		height=f'height: {height}px;'
 
 		class_names=f'class="{class_names}" ' if class_names else ""
-		style=f'style="{style}" ' if style else ""
+		style=f'style="{width}{height}{style}"'
 
 		imgmck=subprocess.run(['magick', asset(filename), '-geometry', f'{w}x{h}', *IM_OPTS, 'JPG:-'], stdout=subprocess.PIPE)
-		return f'''<img {class_names}{style}{width}{height} src="data: image/jpeg; base64, {base64.b64encode(imgmck.stdout).decode()}">'''
+		return f'''<img {class_names}{style} src="data: image/jpeg; base64, {base64.b64encode(imgmck.stdout).decode()}">'''
 
 	def skill(skill_name: str) -> str:
 		imgmck=subprocess.run(['magick', '-background', 'transparent', f'skill_icons/{skill_name}.svg', '-geometry', f'x{16*IMAGE_SCALE}', *IM_OPTS, 'WEBP:-'], stdout=subprocess.PIPE)
@@ -37,13 +37,13 @@ if IMAGE_SCALE:=float(environ.get('IMAGE_SCALE',0)):
 else:
 	def image(filename: str, height: int, width: int=None, *, scale: int=1, class_names='', style='') -> str:
 		mimetype=subprocess.run(['file', '-b', '--mime-type', asset(filename)], stdout=subprocess.PIPE)
-		width=f'width={width} ' if width else ""
-		height=f'height="{height}" '
+		width=f'width: {width}px; ' if width else ""
+		height=f'height: {height}px; '
 		class_names=f'class="{class_names}" ' if class_names else ""
-		style=f'style="{style}" ' if style else ""
+		style=f'style="{width}{height}{style}" '
 
 		with open(asset(filename), 'rb') as f:
-			return f'<img {class_names}{style}{width}{height} src="data: {mimetype.stdout.decode()}; base64, {base64.b64encode(f.read()).decode()}">'
+			return f'<img {class_names}{style} src="data: {mimetype.stdout.decode()}; base64, {base64.b64encode(f.read()).decode()}">'
 
 	def skill(skill_name: str) -> str:
 		try:
